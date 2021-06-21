bdc1a81eb6611be18660d5d2ada72dab6d0e0663
Amaka Eguzoro <eguzoroa@gmail.com>
2021-06-21 09:53:02 +0100

    validated the form

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -7,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+  myForm: FormGroup;
+  constructor(private fb: FormBuilder){}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      holder: ['', Validators.required],
+      cardNumber: ['', Validators.required],
+      expdate: ['', Validators.required],
+      cvv: ['', Validators.required],
+      amount: ['', Validators.required]
+    })
   }
 
+  onSubmit() {
+    console.warn(this.myForm.value);
+    this.myForm.reset();
+  }
+  get holder(){
+    return this.myForm.get('holder')
+  }
+  get cardNumber(){
+    return this.myForm.get('cardNumber')
+  }
+  get expdate(){
+    return this.myForm.get('expdate')
+  }
+  get cvv(){
+    return this.myForm.get('cvv')
+  }
+  get amount(){
+    return this.myForm.get('amount')
+  }
 }
